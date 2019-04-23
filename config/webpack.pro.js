e69248b3d53ec0e7ef7dc94ac15705b39af2c706
johnnyzhang1992 <zq19920306@gmail.com>
2019-04-23 17:29:27 +0800

    use webpack-manifest-plugin

--- config/webpack.pro.js
+++ config/webpack.pro.js
@@ -2,12 +2,15 @@ const merge = require('webpack-merge');
 const base = require("./webpack.base.js");
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const path = require("path");
+// add Mainfest
+var ManifestPlugin = require('webpack-manifest-plugin');
 
 module.exports = merge(base ,{
     mode: 'production',
     plugins: [
         new CleanWebpackPlugin(['dist'],{
             root: path.resolve(__dirname,'../'),
-        })
+        }),
+        new ManifestPlugin()
     ],
 })
